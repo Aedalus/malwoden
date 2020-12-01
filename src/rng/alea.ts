d95ec050d94025141bdee5755b0407211f9c41b3
Alexander Higgins <thealexanderhiggins@gmail.com>
2020-12-01 18:31:36 -0500

    Alea rng nearing completion

--- src/rng/alea.ts
+++ src/rng/alea.ts
@@ -1,6 +1,8 @@
 // https://github.com/nquinlan/better-random-numbers-for-javascript-mirror
 // Johannes Baagøe <baagoe@baagoe.com>, 2010
 
+import { RNG } from "./rng";
+
 function Mash() {
   var n = 0xefc8249d;
 
@@ -22,7 +24,7 @@ function Mash() {
   return mash;
 }
 
-export class AleaRNG {
+export class AleaRNG implements RNG {
   private seed: string;
   private s0 = 0;
   private s1 = 0;
@@ -32,10 +34,10 @@ export class AleaRNG {
   constructor(seed?: string) {
     // Initialize seed if needed
     this.seed = seed === undefined ? new Date().toString() : seed;
-    this.init();
+    this.reset();
   }
 
-  private init() {
+  reset() {
     let mash = Mash();
 
     // Initial mashes
@@ -54,20 +56,44 @@ export class AleaRNG {
     if (this.s2 < 0) this.s2 += 1;
   }
 
-  private next(): number {
+  private step(): number {
     // The dark magic that makes the number generator run
     var t = 2091639 * this.s0 + this.c * 2.3283064365386963e-10; // 2^-32
     this.s0 = this.s1;
     this.s1 = this.s2;
     return (this.s2 = t - (this.c = t | 0));
   }
-
-  uint32(): number {
-    return this.next() * 0x100000000; // 2^32
+  // Returns between [0,1)
+  private nextRand(): number {
+    return this.step() + ((this.step() * 0x200000) | 0) * 1.1102230246251565e-16; // 2^-53
   }
 
-  fract53(): number {
-    return this.next() + ((this.next() * 0x200000) | 0) * 1.1102230246251565e-16; // 2^-53
+  next(min = 0, max = 1): number {
+    return this.nextRand() * (max - min) + min;
+  }
+
+  nextInt(min = 0, max = 100): number {
+    return Math.floor(this.next() * (max - min) + min);
+  }
+
+  nextBoolean(): boolean {
+    return this.nextRand() > 0.5;
+  }
+
+  nextItem<T>(array: T[]): T | undefined {
+    if (array.length === 0) return undefined;
+    const i = this.nextInt(0, array.length);
+    return array[i];
+  }
+
+  shuffle<T>(array: T[]): T[] {
+    const result: T[] = [];
+    const clone = array.slice();
+    while (clone.length) {
+      const index = this.nextInt(0, clone.length);
+      result.push(clone.splice(index, 1)[0]);
+    }
+    return result;
   }
 
   clone(): AleaRNG {
