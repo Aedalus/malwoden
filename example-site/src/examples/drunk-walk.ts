90a0ebe8594e802db84bf5ecb50370e2ebfc0562
infernezor@gmail.com <infernezor@gmail.com>
2020-12-02 06:59:53 -0500

    working on getting it to walk.

--- example-site/src/examples/drunk-walk.ts
+++ example-site/src/examples/drunk-walk.ts
@@ -3,19 +3,31 @@ import { Terminal, Glyph, CharCode, Input, Color, Generation, Util } from "cacti
 
 export default class extends Example {
   Run() {
-    const font = new Terminal.Font("Courier", 24, 15, 24, 1, 24);
-    const terminal = Terminal.Canvas.New(10, 10, font);
+    const terminal = Terminal.Retro.fromURL(80, 50, "font_16.png", 16, 16);
+    const map = new Generation.DrunkardsWalk(80, 50, 2, 1, 1);
+    map.doSimulationStep(10, 10, 3);
     terminal.clear();
-    terminal.writeAt({
-      x: 1,
-      y: 1,
-      text: "foo",
-    });
-    terminal.writeAt({
-      x: 0,
-      y: 0,
-      text: "bar",
-    });
-    terminal.render();
+    for (let x = 0; x < map.table.width; x++) {
+      for (let y = 0; y < map.table.height; y++) {
+        if (map.table.get(x, y) === 2) {
+          terminal.drawCharCode({
+            x: x,
+            y: y,
+            charCode: CharCode.blackSpadeSuit,
+            fore: Color.green,
+          });
+        }
+        //    console.log('position is: ', x, y)
+        //    console.log(map.table.get(x,y));
+        if (map.table.get(x, y) === 1) {
+          terminal.drawCharCode({
+            x: x,
+            y: y,
+            charCode: CharCode.blackSpadeSuit,
+            fore: Color.aqua,
+          });
+        }
+      }
+    }
   }
 }
--- src/generation/drunkards-walk.ts
+++ src/generation/drunkards-walk.ts
@@ -2,7 +2,42 @@ import { Table } from "../util";
 
 export class DrunkardsWalk<T> {
   table: Table<T>;
-  constructor(width: number, height: number) {
+  CurrentPosition: T;
+  Traversed: T;
+  Steps: number;
+
+  constructor(width: number, height: number, CurrentPosition: T, Traversed: T, Steps: number) {
     this.table = new Table(width, height);
+    this.CurrentPosition = CurrentPosition;
+    this.Traversed = Traversed;
+    this.Steps = Steps;
+  }
+  walk(x: number, y: number) {
+    const newMap = new Table<T>(this.table.width, this.table.height);
+    console.log("we are walking", x, y);
+    const ronNum = Math.floor(Math.random() * Math.floor(4));
+    newMap.set(20, 20, this.CurrentPosition);
+    newMap.set(19, 20, this.Traversed);
+    return newMap;
+  }
+
+  doSimulationStep(xInitial: number, yInitial: number, stepsToTake = 1) {
+    const oldMap = this.table;
+    oldMap.set(xInitial, yInitial, this.CurrentPosition);
+    for (let step = 0; step < stepsToTake; step++) {
+      //this just renames the table to prevent 'this' all over the place.
+
+      for (let x = 0; x < oldMap.width; x++) {
+        for (let y = 0; y < oldMap.height; y++) {
+          if (oldMap.get(x, y) === this.CurrentPosition) {
+            this.table = this.walk(x, y);
+          } else {
+            console.log("we could not find an initial position.");
+          }
+        }
+      }
+    }
+
+    //wrap this in a for loop for the length of steps to take at the end.
   }
 }
