20a9269055359a5e4b10e2e63392570e71fc3f63
infernezor@gmail.com <infernezor@gmail.com>
2020-12-04 15:17:39 -0500

    successfuly counts how many times the drunk steps on a tile.

--- src/generation/drunkards-walk.ts
+++ src/generation/drunkards-walk.ts
@@ -1,4 +1,5 @@
 import { Table } from "../util";
+import { isNumber } from "util";
 
 export class DrunkardsWalk {
   table: Table<number>;
@@ -7,15 +8,37 @@ export class DrunkardsWalk {
     this.table = new Table(width, height);
   }
 
-  private checkStep(historicalRecord: [], coveredTileCount: number) {}
+  gethistoricalDataAtX() {
+    //gets the requested historical data at x position.
+  }
+
+  getHistoricalData() {
+    //gets the requested historical data.
+  }
+
+  private addStep(historicalRecord: any, xCord: number, yCord: number) {
+    //check to see if the requested cordinates are unique within historicalRecord. If unique, return true. If not unique return false.
+    const result = historicalRecord.filter(
+      (record: any) => record.xCord === xCord && yCord === record.yCord
+    );
+    console.log(result);
+    if (result.length <= 1) {
+      console.log("we are recording true.");
+      return true;
+    } else {
+      console.log("we are recording false.");
+      return false;
+    }
+  }
 
   doSimulationStep(xInitial: number, yInitial: number, stepsToTake = 1) {
     //constants
     const Map = new Table<number>(this.table.width, this.table.height);
     const currentPosition = 2; //these numbers don't really matter. They need need to hold a num position.
     const Traversed = 1; //these numbers don't really matter. They need need to hold a num position.
-    let coveredTileCount = 0;
+    let coveredTileCount: number = 0;
     let historicalRecord = [];
+    let check: boolean;
     //initial set for the drunk's position.
     Map.set(xInitial, yInitial, currentPosition);
 
@@ -33,23 +56,39 @@ export class DrunkardsWalk {
                 Map.set(x, y, Traversed);
                 historicalRecord.push({ xCord: x, yCord: y });
                 //check to see if this space has been occupied before.
+                check = this.addStep(historicalRecord, x, y);
+                if (check === true) {
+                  coveredTileCount = coveredTileCount + 1;
+                }
                 break;
               case 1:
                 Map.set(x, y + 1, currentPosition);
                 Map.set(x, y, Traversed);
                 historicalRecord.push({ xCord: x, yCord: y });
+                check = this.addStep(historicalRecord, x, y);
+                if (check === true) {
+                  coveredTileCount = coveredTileCount + 1;
+                }
                 //check to see if this space has been occupied before.
                 break;
               case 2:
                 Map.set(x + 1, y, currentPosition);
                 Map.set(x, y, Traversed);
                 historicalRecord.push({ xCord: x, yCord: y });
+                check = this.addStep(historicalRecord, x, y);
+                if (check === true) {
+                  coveredTileCount = coveredTileCount + 1;
+                }
                 //check to see if this space has been occupied before.
                 break;
               case 3:
                 Map.set(x - 1, y, currentPosition);
                 Map.set(x, y, Traversed);
                 historicalRecord.push({ xCord: x, yCord: y });
+                check = this.addStep(historicalRecord, x, y);
+                if (check === true) {
+                  coveredTileCount = coveredTileCount + 1;
+                }
               //check to see if this space has been occupied before.
             }
             //sets x and y to an absurd number so the forloop ends it doesn't get picked a easterly or southerly movement.
