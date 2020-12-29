086b2000dee68d366b5de72d9cd93eb7184d5f12
Alex Higgins <thealexanderhiggins@gmail.com>
2020-12-28 21:42:58 -0500

    Added more unit tests to util classes

--- src/util/line.spec.ts
+++ src/util/line.spec.ts
@@ -2,31 +2,38 @@ import { Line } from "./line";
 
 describe("line", () => {
   it("can create a new line", () => {
-    const line = new Line(1, 1, 2, 2);
+    const line = new Line({ x: 1, y: 1 }, { x: 2, y: 2 });
 
-    expect(line.x1).toBe(1);
-    expect(line.y1).toBe(1);
-
-    expect(line.x2).toBe(2);
-    expect(line.y2).toBe(2);
+    expect(line.v1).toEqual({ x: 1, y: 1 });
+    expect(line.v2).toEqual({ x: 2, y: 2 });
 
     expect(line.getDeltaX()).toBe(1);
     expect(line.getDeltaY()).toBe(1);
   });
 
+  it("can set new points", () => {
+    const line = new Line({ x: 1, y: 1 }, { x: 2, y: 2 });
+
+    expect(line.getDeltaX()).toBe(1);
+    expect(line.getDeltaY()).toBe(1);
+
+    line.v1 = { x: 3, y: 3 };
+    line.v2 = { x: 5, y: 5 };
+
+    expect(line.getDeltaX()).toBe(2);
+    expect(line.getDeltaY()).toBe(2);
+  });
+
   it("can clone a line", () => {
-    const line = new Line(1, 2, 3, 4);
+    const line = new Line({ x: 1, y: 2 }, { x: 3, y: 4 });
     const cloned = line.clone();
 
-    expect(line.x1).toEqual(cloned.x1);
-    expect(line.x2).toEqual(cloned.x2);
-
-    expect(line.y1).toEqual(cloned.y1);
-    expect(line.y2).toEqual(cloned.y2);
+    expect(line.v1).toEqual(cloned.v1);
+    expect(line.v2).toEqual(cloned.v2);
   });
 
   it("can calculate collinear points", () => {
-    const line = new Line(1, 1, 2, 2);
+    const line = new Line({ x: 1, y: 1 }, { x: 2, y: 2 });
 
     // Valid test cases
     const collinearPoints = [
@@ -47,8 +54,15 @@ describe("line", () => {
     }
   });
 
+  it("can calculate collinear lines", () => {
+    const lineA = new Line({ x: 0, y: 0 }, { x: 1, y: 1 });
+    const lineB = new Line({ x: 1, y: 1 }, { x: 3, y: 3 });
+
+    expect(lineA.isLineCollinear(lineB)).toBeTruthy();
+  });
+
   it("can calculate points above", () => {
-    const line = new Line(1, 1, 2, 2);
+    const line = new Line({ x: 1, y: 1 }, { x: 2, y: 2 });
 
     const abovePoints = [
       { x: 2, y: 3 },
