aa4331baa8c5e7dcb44f06d2abc59122e32dd489
Alexander Higgins <TheAlexanderHiggins@gmail.com>
2021-01-05 23:04:58 -0500

    test(input): Added additional tests to input module (#20)

---
+++ src/input/keyboard.spec.ts
@@ -0,0 +1,100 @@
+import { JSDOM } from "jsdom";
+import { KeyboardHandler, KeyboardContext } from "./keyboard";
+import { KeyCode } from "./keycode";
+
+describe("KeyboardHandler", () => {
+  beforeEach(() => {
+    const dom = new JSDOM();
+    //@ts-ignore
+    global.document = dom.window.document;
+    //@ts-ignore
+    global.window = dom.window;
+    //@ts-ignore
+    global.KeyboardEvent = dom.window.KeyboardEvent;
+  });
+
+  it("Will listen to keyboard events", () => {
+    const h = new KeyboardHandler();
+    const c = new KeyboardContext();
+
+    let down = 0;
+    let up = 0;
+
+    c.onDown(KeyCode.A, () => {
+      down++;
+    });
+    c.onUp(KeyCode.A, () => {
+      up++;
+    });
+
+    h.setContext(c);
+
+    var event = new KeyboardEvent("keydown", { keyCode: KeyCode.A });
+    document.dispatchEvent(event);
+
+    expect(down).toEqual(1);
+    expect(up).toEqual(0);
+
+    var event = new KeyboardEvent("keyup", { keyCode: KeyCode.A });
+    document.dispatchEvent(event);
+
+    expect(down).toEqual(1);
+    expect(up).toEqual(1);
+  });
+
+  it("Can set a context", () => {
+    const h = new KeyboardHandler();
+    const c = new KeyboardContext();
+
+    h.setContext(c);
+    expect(h["context"]).toBe(c);
+  });
+
+  it("Can clear a context", () => {
+    const h = new KeyboardHandler();
+    const c = new KeyboardContext();
+
+    h.setContext(c);
+    expect(h["context"]).toBe(c);
+
+    h.clearContext();
+    expect(h["context"]).toBeUndefined();
+  });
+});
+
+describe("KeyboardHandler", () => {
+  it("Can fire onDown", () => {
+    const c = new KeyboardContext();
+
+    let count = 0;
+
+    c.onDown(KeyCode.A, () => count++);
+
+    c.fireOnDown(KeyCode.A);
+    c.fireOnDown(KeyCode.A);
+    c.fireOnDown(KeyCode.A);
+
+    expect(count).toEqual(3);
+  });
+
+  it("Can fire onUp", () => {
+    const c = new KeyboardContext();
+
+    let count = 0;
+
+    c.onUp(KeyCode.A, () => count++);
+
+    c.fireOnUp(KeyCode.A);
+    c.fireOnUp(KeyCode.A);
+    c.fireOnUp(KeyCode.A);
+
+    expect(count).toEqual(3);
+  });
+
+  it("Won't error on unregistered keys", () => {
+    const c = new KeyboardContext();
+
+    c.fireOnUp(KeyCode.A);
+    c.fireOnDown(KeyCode.A);
+  });
+});
