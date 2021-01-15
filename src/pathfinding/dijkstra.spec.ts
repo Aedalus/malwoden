66094710e526c8d7f315358e6532fc09b4f32e61
infernezor@gmail.com <infernezor@gmail.com>
2021-01-14 23:29:39 -0600

    finished basic pathfind.

--- src/pathfinding/dijkstra.spec.ts
+++ src/pathfinding/dijkstra.spec.ts
@@ -6,6 +6,12 @@ describe("dijkstra", () =>
   it("recognize as start = end case", () => {
     console.log("running test");
     const a = new Dijkstra({});
-    const path = a.compute({ x: 2, y: 3 }, { x: 3, y: 3 });
-    expect(path).toEqual([{ x: 3, y: 3 }]);
+    const path = a.compute({ x: 2, y: 3 }, { x: 3, y: 6 });
+    expect(path).toEqual([
+      { x: 2, y: 3 },
+      { x: 2, y: 4 },
+      { x: 2, y: 5 },
+      { x: 2, y: 6 },
+      { x: 3, y: 6 },
+    ]);
   }));
--- src/pathfinding/dijkstra.ts
+++ src/pathfinding/dijkstra.ts
@@ -1,4 +1,5 @@
 import { Vector2 } from "../util";
+import * as Math from "../math";
 
 interface dijkstraConfig {
   isBlockedCallback?: IsBlockedCallback;
@@ -6,36 +7,65 @@ interface dijkstraConfig {
 
 type IsBlockedCallback = (v: Vector2) => boolean;
 
-interface dijkstraNode {
-  v: Vector2;
-}
-
 export class Dijkstra {
   private readonly isBlocked?: IsBlockedCallback;
+
   constructor(config: dijkstraConfig) {
     this.isBlocked = config.isBlockedCallback;
   }
+
   compute(initial: Vector2, goal: Vector2): Vector2[] | undefined {
     //unpackage config.
     const cameFrom = new Map<string, string>();
-    let processing = [initial];
-    console.log("the log is: ", processing);
-    console.log("the log is: ", processing.length);
+    let processed: string[] = [];
+    let processing: Vector2[] = [];
+    processing.push(initial);
 
     while (processing.length !== 0) {
       //shift the last node off the array.
-      const current = processing.shift();
-      console.log("the current processing element is: ", current);
+      const current = processing.shift()!;
       // check to see if the space is where you need to be. If so, exit loop.
-      if (current === goal) {
-        break;
+      if (Math.Vector.areEqual(current, goal)) {
+        let curStr = `${current.x}:${current.y}`;
+        const totalPath: Vector2[] = [current];
+
+        while (cameFrom.has(curStr)) {
+          //infinate loop here.
+          const prevStr = cameFrom.get(curStr)!;
+          const [x, y] = prevStr.split(":");
+          curStr = prevStr;
+          totalPath.unshift({
+            x: Number.parseInt(x),
+            y: Number.parseInt(y),
+          });
+        }
+        return totalPath;
       }
-      //look at the adjacent spots.
+
       //check adjactent spots to see if they are either blocked or don't exist.
+      const neighbors = [
+        { x: current.x - 1, y: current.y },
+        { x: current.x + 1, y: current.y },
+        { x: current.x, y: current.y - 1 },
+        { x: current.x, y: current.y + 1 },
+      ];
 
-      // push in the four corners into processing if available on the table.
+      //set up for prcessing the nieghbors to see if they've been checked before.
+      let processedNeighbors = [];
 
-      //return an array of vector2s that corrospond to the path.
+      for (let n of neighbors) {
+        const check = processed.includes(`${n.x}:${n.y}`);
+        if (check === false) {
+          processedNeighbors.push(n);
+        }
+      }
+
+      for (let n of processedNeighbors) {
+        //should check if it is in bounds?
+        processing.push(n);
+        cameFrom.set(`${n.x}:${n.y}`, `${current!.x}:${current!.y}`); //putting a space here makes the program work when it should break it. Why?
+      }
+      processed.push(`${current.x}:${current.y}`);
     }
 
     return undefined;
