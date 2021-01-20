044a98b8dffeb1b930de88550782bad1cdaef93d
infernezor@gmail.com <infernezor@gmail.com>
2021-01-20 00:20:22 -0600

    revamped dijkstra with Alex's help.

--- src/pathfinding/dijkstra.ts
+++ src/pathfinding/dijkstra.ts
@@ -21,20 +21,27 @@ export class Dijkstra {
   compute(initial: Vector2, goal: Vector2): Vector2[] | undefined {
     //unpackage config.
     const cameFrom = new Map<string, string>();
-    let processed: string[] = [];
-    let processing: Vector2[] = [];
-    processing.push(initial);
+    const processed: Set<string> = new Set();
+    const processing: Vector2[] = [initial];
+    cameFrom.set(`${initial.x}:${initial.y}`, "");
 
     while (processing.length !== 0) {
       //shift the last node off the array.
       const current = processing.shift()!;
+
+      //short circut
+      if (processed.has(`${current.x}:${current.y}`)) {
+        continue;
+      } else {
+        processed.add(`${current.x}:${current.y}`);
+      }
+
       // check to see if the space is where you need to be. If so, exit loop.
       if (Math.Vector.areEqual(current, goal)) {
         let curStr = `${current.x}:${current.y}`;
         const totalPath: Vector2[] = [current];
 
-        while (cameFrom.has(curStr)) {
-          //infinate loop here.
+        while (cameFrom.get(curStr)) {
           const prevStr = cameFrom.get(curStr)!;
           const [x, y] = prevStr.split(":");
           curStr = prevStr;
@@ -43,38 +50,26 @@ export class Dijkstra {
             y: Number.parseInt(y),
           });
         }
+
         return totalPath;
       }
 
-      //check adjactent spots to see if they are either blocked or don't exist.
       let neighbors =
         this.topology === "four"
           ? getRing4(current.x, current.y, 1)
           : getRing8(current.x, current.y, 1);
 
-      //set up for prcessing the nieghbors to see if they've been checked before.
-      let processedNeighbors = [];
-
-      for (let n of neighbors) {
-        const check = processed.includes(`${n.x}:${n.y}`);
-        if (check === false) {
-          processedNeighbors.push(n);
-        }
-      }
-
       if (this.isBlocked) {
         neighbors = neighbors.filter((v) => this.isBlocked!(v) === false);
       }
 
-      for (let n of processedNeighbors) {
-        //should check if it is in bounds?
+      for (let n of neighbors) {
         processing.push(n);
-        cameFrom.set(`${n.x}:${n.y}`, `${current!.x}:${current!.y}`); //putting a space here makes the program work when it should break it. Why?
+        if (cameFrom.has(`${n.x}:${n.y}`) === false) {
+          cameFrom.set(`${n.x}:${n.y}`, `${current!.x}:${current!.y}`);
+        }
       }
-
-      processed.push(`${current.x}:${current.y}`);
     }
-
     return undefined;
   }
 }
