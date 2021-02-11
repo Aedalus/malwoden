7f1d718661422ba7d96271d7cd9ec048ee385b11
Alexander Higgins <thealexanderhiggins@gmail.com>
2021-02-11 17:12:34 -0500

    fixed merge conflicts

--- example-site/src/examples/input/mouse-input-font.tsx
+++ example-site/src/examples/input/mouse-input-font.tsx
@@ -3,17 +3,10 @@ import React, { useEffect } from "react";
 import { CharCode, Color, Terminal, Input } from "malwoden";
 
 const MouseInput = () => {
-<<<<<<< HEAD:example-site/src/examples/input/mouse-input-font.tsx
   const requestRef = React.useRef<number>(NaN);
 
   useEffect(() => {
     const mountNode = document.getElementById("example")!;
-=======
-  const requestRef = React.useRef<number>();
-
-  useEffect(() => {
-    const mountNode = document.getElementById("example");
->>>>>>> 89d998a6a685a92fc85cd4b9c38d6767791bc786:example-site/examples/input/mouse-input-font.tsx
     const font = new Terminal.Font("Courier", 24, 15, 24, 1, 20);
     const terminal = new Terminal.CanvasTerminal({
       width: 20,
